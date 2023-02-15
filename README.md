d630d9d5aea4c22eb741888dac3e281455e944a8
NickC91 <nicola.calza91@gmail.com>
2023-02-16 00:01:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Counter-app 
 
-Una semplice applicazione che tramite due pulsanti più (+) e meno (-) andiamo ad incrementare e a ridurre un valore inizialmente impostato a zero (0)
+Una semplice applicazione che tramite due pulsanti più (+) e meno (-) andiamo ad incrementare e a ridurre un valore inizialmente impostato a zero (0) con la possibilità di azzerare il numero tramite il pulsante Resetta.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,67 @@
-console.log('OK')
+//Creazione elementi
+const node = document.createElement("div")
+
+const firstNode = document.createElement("div")
+const h1 = document.createElement("h1")
+const h2 = document.createElement("h2")
+
+const secondNode = document.createElement("div")
+const plus = document.createElement("span")
+const reset = document.createElement("span")
+const minus = document.createElement("span")
+
+//Associazione
+node.classList = "container"
+
+firstNode.classList = "wrapper"
+h1.textContent = "CONTATORE"
+h2.id = "valore"
+h2.textContent = '0'
+
+secondNode.classList = "flex-container"
+plus.id = "piu"
+plus.textContent = "+"
+
+minus.id = "meno"
+minus.textContent = "-"
+
+reset.id = "resetta"
+reset.textContent = "RESET"
+
+//Gestione DOM
+document.body.appendChild(node)
+
+node.appendChild(firstNode)
+firstNode.appendChild(h1)
+firstNode.appendChild(h2)
+
+node.appendChild(secondNode)
+secondNode.appendChild(plus)
+secondNode.appendChild(reset)
+secondNode.appendChild(minus)
+
+//Start iniziale
+let value = 0
+
+// Cambio valore
+function changeValue() {
+    return valore.textContent = value
+}
+
+//Logica incremento
+piu.addEventListener('click', () => {
+    value = value + 1
+    changeValue()
+})
+
+//Logica riduzione
+meno.addEventListener('click', () => {
+    value = value - 1
+    changeValue()
+})
+
+//Logica reset
+resetta.addEventListener('click', () => {
+    value = 0
+    changeValue()
+})
